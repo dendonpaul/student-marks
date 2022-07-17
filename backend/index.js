2216c95d1116dad3f9f8cb18ca527c00ebb5c445
dendonpaul <dendonpaul@gmail.com>
2022-07-17 12:54:11 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.get("/hello", (req, res) => {
+  console.log("hello");
+});
+
 app.listen(5001, () => console.log("Server running at 5001"));
